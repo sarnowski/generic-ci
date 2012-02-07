46a34ab8ecec10918cc4488f7c4588eb882777ec
Florian Purchess <purchess@cosmocode.de>
2012-02-07 15:10:49 +0100

    implemented own cookie-algo

--- share/js/gitce.js
+++ share/js/gitce.js
@@ -39,43 +39,18 @@ var GITCE = {
         return cookieValue === null ? null : $.parseJSON(cookieValue);
     },
 
-    /**
-     * Adapted from jQuery Cookie plugin
-     * Copyright (c) 2010 Klaus Hartl (stilbuero.de)
-     */
-    cookie:function (key, value, options) {
-        if (arguments.length > 1 && (!/Object/.test(Object.prototype.toString.call(value)) || value === null || value === undefined)) {
-            options = $.extend({}, options);
-
-            if (value === null || value === undefined) {
-                options.expires = -1;
+    cookie:function (key, value) {
+        if (arguments.length == 1) {
+            var pairs = document.cookie.split('; ');
+            for (var i = 0, pair; pair = pairs[i] && pairs[i].split('='); i++) {
+                if (decodeURIComponent(pair[0]) === key) {
+                    return decodeURIComponent(pair[1] || '');
+                }
             }
+            return null;
 
-            if (typeof options.expires === 'number') {
-                var days = options.expires, t = options.expires = new Date();
-                t.setDate(t.getDate() + days);
-            }
-
-            value = String(value);
-
-            return (document.cookie = [
-                encodeURIComponent(key), '=', options.raw ? value : encodeURIComponent(value),
-                options.expires ? '; expires=' + options.expires.toUTCString() : '',
-                options.path ? '; path=' + options.path : '',
-                options.domain ? '; domain=' + options.domain : '',
-                options.secure ? '; secure' : ''
-            ].join(''));
+        } else {
+            document.cookie = encodeURIComponent(key) + '=' + encodeURIComponent(value);
         }
-
-        options = value || {};
-        var decode = options.raw ? function (s) {
-            return s;
-        } : decodeURIComponent;
-
-        var pairs = document.cookie.split('; ');
-        for (var i = 0, pair; pair = pairs[i] && pairs[i].split('='); i++) {
-            if (decode(pair[0]) === key) return decode(pair[1] || '');
-        }
-        return null;
     }
 };
