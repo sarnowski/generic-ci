dd57e937fc81c4be110311d84640c3d10155dfac
Tobias Sarnowski <sarnowski@new-thoughts.org>
2012-03-18 14:52:57 +0100

    made web export a bit "nicer"

--- lib/web.sh
+++ lib/web.sh
@@ -25,6 +25,123 @@ running() {
 }
 
 #
+# CSS
+#
+cat > $WEB/genci.css << "EOF"
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed, 
+figure, figcaption, footer, header, hgroup, 
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+	margin: 0;
+	padding: 0;
+	border: 0;
+	font-size: 100%;
+	font: inherit;
+	font-family: Arial, sans-serif;
+	vertical-align: baseline;
+}
+article, aside, details, figcaption, figure, 
+footer, header, hgroup, menu, nav, section {
+	display: block;
+}
+body {
+	line-height: 1;
+}
+ol, ul {
+	list-style: none;
+}
+blockquote, q {
+	quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+	content: '';
+	content: none;
+}
+table {
+	border-collapse: collapse;
+	border-spacing: 0;
+}
+
+
+body {
+	padding: 20px 10px;
+	background-color: white;
+}
+h1 {
+	font-size: 32px;
+	font-weight: bold;
+	margin-bottom: 20px;
+}
+h2 {
+	margin-bottom: 30px;
+}
+td, th {
+	padding: 4px 15px;
+	background-color: lightgrey;
+	text-align: left;
+	border: 1px solid white;
+}
+a {
+	text-decoration: none;
+}
+th a {
+	font-weight: bold;
+	color: black;
+}
+.ok {
+	color: black;
+}
+.broken {
+	color: red;
+}
+.running {
+	color: darkyellow;
+}
+th.ok {
+	color: black;
+	background-color: #0d0;
+}
+th.broken {
+	color: black;
+	background-color: red;
+}
+th.running {
+	color: black;
+	background-color: yellow;
+}
+th.inactive {
+	color: black;
+	background-color: grey;
+}
+p {
+	margin-top: 20px;
+	font-size: 10px;
+}
+ul a {
+	text-decoration: none;
+	font-weight: bold;
+}
+li > ul {
+	margin: 0 0 10px 70px;
+}
+
+li > ul li {
+	list-style-type: disc;
+	margin: 5px 0;
+}
+EOF
+
+#
 # INDEX
 #
 cat > $WEB/index.html << "EOF"
@@ -37,41 +154,7 @@ echo "<title>$CONFIG - generic-ci</title>" >> $WEB/index.html
 
 cat >> $WEB/index.html << "EOF"
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<style type="text/css">
-			td, th {
-				padding: 4px 15px;
-				background-color: lightgrey;
-				text-align: left;
-			}
-			a {
-				text-decoration: none;
-			}
-			th a {
-				text-weight: bold;
-				color: black;
-			}
-			th.ok {
-				background-color: green;
-			}
-			a.ok {
-				color: green;
-			}
-			th.broken {
-				background-color: red;
-			}
-			a.broken {
-				color: red;
-			}
-			th.running {
-				background-color: yellow;
-			}
-			a.running {
-				color: darkyellow;
-			}
-			th.inactive {
-				background-color: grey;
-			}
-		</style>
+		<link rel="stylesheet" type="text/css" href="genci.css">
 	</head>
 	<body>
 EOF
@@ -93,7 +176,7 @@ $GENCI status $CONFIG | while read line; do
 		bhealth="broken"
 		[ "$(cat $BUILDS/master/build/$(($last - $cnt))/result 2>/dev/null)" = "0" ] && bhealth="ok"
 
-		echo "<td><a class=\"$health\" href=\"$branch-$(($last - $cnt)).txt\">#$(($last - $cnt))</a></td>" >> $WEB/index.html
+		echo "<td><a class=\"$bhealth\" href=\"$branch-$(($last - $cnt)).txt\">#$(($last - $cnt))</a></td>" >> $WEB/index.html
 		cnt=$(($cnt + 1))
 	done
 	echo "</tr>" >> $WEB/index.html
@@ -101,6 +184,7 @@ done
 
 cat >> $WEB/index.html << "EOF"
 		</table>
+		<p>Builds powered by <a href="https://www.new-thoughts.org/generic-ci/" target="_blank">generic-ci</a>.</p>
 	</body>
 </html>
 EOF
@@ -126,24 +210,13 @@ EOF
 
 	cat >> $WEB/$branch.html << "EOF"
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<style type="text/css">
-			ul a {
-				text-decoration: none;
-				font-weight: bold;
-			}
-			.ok {
-				color: green;
-			}
-			.broken {
-				color: red;
-			}
-		</style>
+		<link rel="stylesheet" type="text/css" href="genci.css">
 	</head>
 	<body>
 EOF
 
 	echo "<h1>$CONFIG / $branch builds history</h1>" >> $WEB/$branch.html
-	echo "<a href=\"index.html\">back to overview</a>" >> $WEB/$branch.html
+	echo "<h2><a href=\"index.html\">back to overview</a></h2>" >> $WEB/$branch.html
 
 	echo "<ul>" >> $WEB/$branch.html
 	build=$last
@@ -171,6 +244,7 @@ EOF
 	echo "</ul>" >> $WEB/$branch.html
 
 	cat >> $WEB/$branch.html << "EOF"
+		<p>Builds powered by <a href="https://www.new-thoughts.org/generic-ci/" target="_blank">generic-ci</a>.</p>
 	</body>
 </html>
 EOF
@@ -198,7 +272,7 @@ EOF
 				background-color: red;
 			}
 			.ok {
-				background-color: green;
+				background-color: #0d0;
 			}
 			a {
 				font-weight: bold;
