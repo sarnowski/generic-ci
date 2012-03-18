e632c9fc9bfa47883d1752acd1bd3393e6caeb97
Tobias Sarnowski <sarnowski@new-thoughts.org>
2012-03-18 18:43:03 +0100

    added OpenBSD packaging support

--- release.sh
+++ release.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 cd $(dirname $0)
-
-# run tests before
-./test.sh || exit $?
+root=$(pwd)
 
 # resolve version
 GIT_DIR=.git
 [ ! -z "$GENCI_REPOSITORY" ] && GIT_DIR=$GENCI_REPOSITORY
 
-VERSION=$(git --git-dir=$GIT_DIR describe --tags $GENCI_BUILD_SHA1)
+REF=$GENCI_BUILD_SHA1
+[ -z "$REF" ] && REF="HEAD"
+
+VERSION=$(git --git-dir=$GIT_DIR describe --tags $REF)
 if [ -z "$VERSION" ]; then
 	echo "Cannot retrieve version; aborting release!" >&2
 	exit 1
@@ -16,7 +17,7 @@ fi
 
 # generate user tarball
 echo "Building user tarball..."
-git archive --format=tar --prefix="generic-ci-$VERSION/" $VERSION | gzip -9 > generic-ci-user-$VERSION.tar.gz
+git archive --format=tar --prefix="generic-ci-$VERSION/" $REF | gzip -9 > generic-ci-user-$VERSION.tar.gz || exit $?
 
 # a temporary directory for the package
 TMP=/tmp/genci-$(date +%s)
@@ -38,20 +39,53 @@ echo $VERSION > $TMP/usr/local/share/doc/genci/VERSION
 cp -r examples $TMP/usr/local/share/examples/genci
 
 # build server tarball
-echo "Building generic tarball..."
+echo "Building server tarball..."
 TAR=generic-ci-server-$VERSION.tar.gz
 tar czf $TAR -C $TMP usr || exit $?
+
+# build openbsd package
+if [ "$(uname)" = "OpenBSD" ] && [ -d /usr/ports/infrastructure ] && [ $(id -u) -eq 0 ]; then
+	echo "Building OpenBSD package..."
+
+	if [ ! -z "$(echo $VERSION | grep '\-g')" ]; then
+		OVERSION=$(echo $VERSION | sed 's/-.*//')
+		echo "Rewriting version to $OVERSION"
+	else
+		OVERSION=$VERSION
+	fi
+
+	mkdir -p $TMP/devel/generic-ci-openbsd
+	sed "s/VERSION/$OVERSION/g" openbsd/Makefile > $TMP/devel/generic-ci-openbsd/Makefile
+	mkdir -p $TMP/devel/generic-ci-openbsd/pkg
+	cp README $TMP/devel/generic-ci-openbsd/pkg/DESCR
+
+	git archive --format=tar --prefix="generic-ci-openbsd-$OVERSION/" $REF | gzip -9 > /usr/ports/distfiles/generic-ci-openbsd-$OVERSION.tar.gz || exit $?
+
+	export PORTSDIR_PATH=/usr/ports:$TMP
+	cd $TMP/devel/generic-ci-openbsd
+	make clean || exit $?
+	make makesum || exit $?
+	make fake || exit $?
+	make plist || exit $?
+	make package || exit $?
+	cp /usr/ports/packages/no-arch/generic-ci-openbsd-$OVERSION.tgz $root
+	make clean || exit $?
+
+	cd $root
+else
+	echo "Skipping OpenBSD package..."
+fi
 
 # build debian package if possible
 which dpkg-deb >/dev/null 2>&1
 if [ $? -eq 0 ]; then
-	echo "Building debian package..."
+	echo "Building Debian package..."
 	cp -r DEBIAN $TMP
 	sed "s/#VERSION#/$VERSION/g" -i $TMP/DEBIAN/control
 	DEB=generic-ci-$VERSION-all.deb
 	fakeroot dpkg-deb -b $TMP $DEB > /dev/null || exit $?
 else
-	echo "Skipping debian package..."
+	echo "Skipping Debian package..."
 fi
 
 # cleanup
