e6cd7cc8b3cb6bcd20e1a6cb42be6fcc3d7d0c85
Tobias Sarnowski <tobias@trustedco.de>
2012-03-27 10:04:36 +0200

    made the artifact discovery more powerful

--- examples/hooks/artifacts.sh
+++ examples/hooks/artifacts.sh
@@ -3,15 +3,33 @@
 ARTIFACTS_DIR=$GENCI_BUILD_DIR/artifacts
 mkdir -p $ARTIFACTS_DIR
 
-cd $GENCI_BUILD_WORK_DIR
-
 if [ ! -z "$ARTIFACTS" ] && [ -d $GENCI_BUILD_WORK_DIR ]; then
+	cd $GENCI_BUILD_WORK_DIR
+
 	echo "Collecting artifacts..."
 	for artifact in $ARTIFACTS; do
-		for file in $(ls $artifact); do
-			echo "  $file"
-			mkdir -p $(dirname $ARTIFACTS_DIR/$file)
-			cp $file $ARTIFACTS_DIR/$file
-		done
+		if [ -d "$artifact" ]; then
+			echo "  dir  $artifact"
+			cp -r "$artifact" $ARTIFACTS_DIR
+		elif [ -f "$artifact" ]; then
+			echo "  file $artifact"
+			cp "$artifact" $ARTIFACTS_DIR
+		else
+			echo "  glob $artifact"
+			for file in $(ls $artifact); do
+				if [ -d "$file" ]; then
+					echo "    dir  $file"
+					mkdir -p $ARTIFACTS_DIR/$file
+				else
+					echo "    file $file"
+					mkdir -p $(dirname $ARTIFACTS_DIR/$file)
+					cp $file $ARTIFACTS_DIR/$file
+				fi
+			done
+		fi
 	done
+	exit 0
+else
+	echo "Error: cannot collect artifacts, no artifacts configured or work directory vanished!" >&2
+	exit 1
 fi
