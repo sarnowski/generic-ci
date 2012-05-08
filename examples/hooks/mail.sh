75f5b6220dbba303c5fc6124e71ead7c82029f18
Tobias Sarnowski <sarnowski@cosmocode.de>
2012-05-08 10:07:51 +0200

    improved mail hook with PANIC and BRANCHES configs

--- examples/hooks/mail.sh
+++ examples/hooks/mail.sh
@@ -12,6 +12,19 @@ if [ "$GENCI_BUILD_NUMBER" -eq 0 ]; then
 	exit 0
 fi
 
+if [ ! -z "$MAIL_BRANCHES" ]; then
+	skip=1
+	for white in $MAIL_BRANCHES; do
+		if [ "$GENCI_BRANCH" = "$white" ]; then
+			skip=0
+		fi
+	done
+	if [ $skip -eq 1 ]; then
+		echo "Skipping mailing for the branch (not in mail whitelist)."
+		exit 0
+	fi
+fi
+
 # only if we broke the build
 R=$GENCI_BUILD_RESULT
 
@@ -62,40 +75,58 @@ fi
 
 # send failure mails
 if [ $R -ne 0 ]; then
-	# append git and build log
-	echo "GIT HISTORY ($GENCI_BUILD_OLD_SHA1..$GENCI_BUILD_SHA1)" >> $BODY
-	echo "=============================================================================" >> $BODY
-	git --git-dir=$GENCI_REPOSITORY log --oneline $GENCI_BUILD_OLD_SHA1..$GENCI_BUILD_SHA1 >> $BODY
-	echo >> $BODY
-	echo >> $BODY
 
-	if [ ! -z "$GENCI_BUILD_LOG" ]; then
-		echo "BUILD LOG" >> $BODY
-		echo "=============================================================================" >> $BODY
-		cat $GENCI_BUILD_LOG >> $BODY
-		echo >> $BODY
-		echo >> $BODY
+	[ -z "$MAIL_PANIC_LIMIT" ] && MAIL_PANIC_LIMIT=0
+
+	# panic mail check
+	if [ ! -z "$MAIL_PANIC_MAIL" ]; then
+		mail_count=$(git --git-dir=$GENCI_REPOSITORY log --format="%ae|%an" $GENCI_BUILD_OLD_SHA1..$GENCI_BUILD_SHA1 | sort | uniq | wc -l)
+	else
+		mail_count=0
 	fi
 
-	echo "Thank you!" >> $BODY
+	if [ $mail_count -gt $MAIL_PANIC_LIMIT ] && [ $MAIL_PANIC_LIMIT -ne 0 ]; then
+		# PANIC!
+		echo "Too many authors ($mail_count)! Sending one mail to $MAIL_PANIC_MAIL..."
+		echo "$GENCI_BUILD_ID failed and there were too many authors involved to send them mails!" \
+			| mail -s "[$GENCI_CONFIG] $GENCI_BUILD_ID failed with too many authors" $MAIL_PANIC_MAIL
 
+	else
 
-	# send to authors
-	git --git-dir=$GENCI_REPOSITORY log --format="%ae|%an" $GENCI_BUILD_OLD_SHA1..$GENCI_BUILD_SHA1 | sort | uniq | while read line; do
-		email=$(echo $line | cut -d'|' -f1)
-		name=$(echo $line | cut -d'|' -f2)
+		# append git and build log
+		echo "GIT HISTORY ($GENCI_BUILD_OLD_SHA1..$GENCI_BUILD_SHA1)" >> $BODY
+		echo "=============================================================================" >> $BODY
+		git --git-dir=$GENCI_REPOSITORY log --oneline $GENCI_BUILD_OLD_SHA1..$GENCI_BUILD_SHA1 >> $BODY
+		echo >> $BODY
+		echo >> $BODY
 
-		echo "Sending E-Mail to $name <$email>..."
-		cat $BODY \
-			| sed "s/%NAME%/$name/g" \
-			| sed "s/%BUILD_ID%/$GENCI_BUILD_ID/g" \
-			| mail -s "$TITLE" $email
-
-		# remember authors for later success message
-		if [ ! -f $FAILAUTHORS ] || [ -z "$(grep $email $FAILAUTHORS)" ]; then
-			echo "$email|$name" >> $FAILAUTHORS
+		if [ ! -z "$GENCI_BUILD_LOG" ]; then
+			echo "BUILD LOG" >> $BODY
+			echo "=============================================================================" >> $BODY
+			cat $GENCI_BUILD_LOG >> $BODY
+			echo >> $BODY
+			echo >> $BODY
 		fi
-	done
+
+		echo "Thank you!" >> $BODY
+
+		# send to authors
+		git --git-dir=$GENCI_REPOSITORY log --format="%ae|%an" $GENCI_BUILD_OLD_SHA1..$GENCI_BUILD_SHA1 | sort | uniq | while read line; do
+			email=$(echo $line | cut -d'|' -f1)
+			name=$(echo $line | cut -d'|' -f2)
+
+			echo "Sending E-Mail to $name <$email>..."
+			cat $BODY \
+				| sed "s/%NAME%/$name/g" \
+				| sed "s/%BUILD_ID%/$GENCI_BUILD_ID/g" \
+				| mail -s "$TITLE" $email
+
+			# remember authors for later success message
+			if [ ! -f $FAILAUTHORS ] || [ -z "$(grep $email $FAILAUTHORS)" ]; then
+				echo "$email|$name" >> $FAILAUTHORS
+			fi
+		done
+	fi
 fi
 
 # cleanup
